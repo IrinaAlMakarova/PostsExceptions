96c9c1a36afc9762220e49fadde44b6289157713
Irina Makarova <irina.makarova@bk.ru>
2024-03-23 20:58:49 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,177 @@
+package ru.netology
+
+import ru.netology.WallService.add
+
+class Likes
+class Donut
+class Thread
+
+data class Post(
+    var id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val createdBy: Int?, // Nullable
+    val date: Int,
+    val text: String,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
+    val friendsOnly: Int,
+    val copyright: String,
+    val postType: String,
+    val signerId: Int,
+    val canPin: Int,
+    val canDelete: Boolean,
+    val canEdit: Int,
+    val isPinned: Int,
+    val markedAsAds: Boolean,
+    val isFavorite: Boolean,
+    val postponedId: Int,
+    var comments: Comment?, // Nullable
+    var likes: Likes?, // Nullable
+    val attachments: Array<Attachment> = emptyArray()
+)
+
+class Comment(
+    val id: Int,
+    val fromId: Int,
+    val date: Int,
+    val text: String,
+    var donut: Donut,
+    val replyToUser: Int,
+    val replyToComment: Int,
+    var thread: Thread,
+    val attachments: Array<Attachment> = emptyArray()
+)
+
+object WallService {
+    private var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
+    private var recordId: Int = 0
+
+    fun clear() {
+        posts = emptyArray()
+        recordId = 0 // сброс счетчика для id постов
+    }
+
+    // добавление возможности комментирования
+    //Функция сначала должна проверить, существует ли в массиве posts пост с ID равным postId. Если существует, то добавить комментарий в массив comments и вернуть только что добавленный комментарий.
+    // Если не существует, выкинуть исключение PostNotFoundException.
+    fun createComment(postId: Int, comment: Comment): Comment {
+        for (post in posts) {
+            if (post.id == postId) {
+                comments += comment
+                return comment
+            }
+        }
+        return TODO("PostNotFoundException")
+    }
+
+    fun add(post: Post): Post {
+        recordId++
+        post.id = recordId
+        posts += post
+        return posts.last()
+    }
+
+    fun update(postNew: Post): Boolean {
+        var flag: Boolean = false
+        for (post in posts) {
+            if (postNew.id == post.id) {
+                var post = postNew
+                flag = true
+            }
+        }
+        return flag
+    }
+}
+
+
+class Photo(
+    val id: Int,
+    val album_id: Int,
+    val owner_id: Int,
+    val user_id: Int,
+    val text: String
+)
+
+class Audio(
+    val id: Int,
+    val owner_id: Int,
+    val artist: String,
+    val title: String,
+    val duration: Int
+)
+
+class Video(
+    val id: Int,
+    val owner_id: Int,
+    val title: String,
+    val description: String,
+    val duration: Int
+)
+
+class File(
+    val id: Int,
+    val owner_id: Int,
+    val title: String,
+    val size: Int,
+    val ext: String
+)
+
+class Link(
+    val url: String,
+    val title: String,
+    val caption: String,
+    val description: String,
+    var photo: Photo
+)
+
+interface Attachment {
+    val type: String
+}
+
+class PhotoAttachment(
+    override val type: String,
+    var photo: Photo
+) : Attachment {
+}
+
+class AudioAttachment(
+    override val type: String,
+    var audio: Audio
+) : Attachment {
+}
+
+class VideoAttachment(
+    override val type: String,
+    var video: Video
+) : Attachment {
+}
+
+class FileAttachment(
+    override val type: String,
+    var file: File
+) : Attachment {
+}
+
+class LinkAttachment(
+    override val type: String,
+    var link: Link
+) : Attachment {
+}
+
+fun main() {
+    var comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+    var post =
+        Post(1, 1, 1, 2, 1234, "Text", 3, 4, 1, "Ist", "post", 13, 1, true, 1, 1, true, false, 14, comment, Likes())
+    println(add(post))
+    WallService.createComment(1, comment)
+    WallService.createComment(2, comment)
+
+
+    comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+    post =
+        Post(1, 2, 1, 2, 1234, "Text", 3, 4, 1, "Ist", "post", 13, 1, true, 1, 1, true, false, 14, comment, Likes())
+    println(add(post))
+}
+
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,347 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import ru.netology.*
+import ru.netology.Thread
+
+class WallServiceTest {
+
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
+
+    @Test
+    fun update() {
+        // создаём целевой сервис
+        val service = ru.netology.WallService
+        var comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+        // заполняем несколькими постами
+        service.add(
+            Post(
+                1,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        service.add(
+            Post(
+                2,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        service.add(
+            Post(
+                3,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        // создаём информацию об обновлении
+        val update = Post(
+            1,
+            1,
+            1,
+            2,
+            1234,
+            "Text1",
+            3,
+            4,
+            1,
+            "Ist",
+            "post",
+            13,
+            1,
+            true,
+            1,
+            1,
+            true,
+            false,
+            14,
+            comment,
+            Likes()
+        )
+
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertTrue(result)
+    }
+
+    @Test
+    fun update1() {
+        // создаём целевой сервис
+        val service = ru.netology.WallService
+        var comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+        // заполняем несколькими постами
+        service.add(
+            Post(
+                5,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        service.add(
+            Post(
+                6,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        service.add(
+            Post(
+                7,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        // создаём информацию об обновлении
+        val update = Post(
+            4,
+            1,
+            1,
+            2,
+            1234,
+            "Text1",
+            3,
+            4,
+            1,
+            "Ist",
+            "post",
+            13,
+            1,
+            true,
+            1,
+            1,
+            true,
+            false,
+            14,
+            comment,
+            Likes()
+        )
+
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertFalse(result)
+    }
+
+    @Test
+    fun add() {
+        // создаём целевой сервис
+        val service = ru.netology.WallService
+        var comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+        // заполняем несколькими постами
+        val result = service.add(
+            Post(
+                7,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        ).id
+
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun createComment1() {
+        // создаём целевой сервис
+        val service = ru.netology.WallService
+        var comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+        // заполняем несколькими постами
+        service.add(
+            Post(
+                1,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+
+        val result = if(service.createComment(1,comment) == comment) true else false
+        assertTrue(result)
+    }
+
+    class PostNotFoundException: Exception()
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        // здесь код с вызовом функции, которая должна выкинуть PostNotFoundException
+        // создаём целевой сервис
+        val service = ru.netology.WallService
+        var comment = Comment(1,2,3,"Com", Donut(), 1,1, Thread());
+        // заполняем несколькими постами
+        service.add(
+            Post(
+                1,
+                1,
+                1,
+                2,
+                1234,
+                "Text",
+                3,
+                4,
+                1,
+                "Ist",
+                "post",
+                13,
+                1,
+                true,
+                1,
+                1,
+                true,
+                false,
+                14,
+                comment,
+                Likes()
+            )
+        )
+        service.createComment(1, comment)
+    }
+}
